56c73fd16f2cae9dda30c7eba54aac40f570b038
Weber588 <idonthavetotellyoumyname@gmail.com>
2024-01-09 22:58:37 +1100

    Fixed hellborne enchant

--- src/main/java/wbs/enchants/enchantment/HellborneEnchant.java
+++ src/main/java/wbs/enchants/enchantment/HellborneEnchant.java
@@ -1,16 +1,20 @@
 package wbs.enchants.enchantment;
 
 import me.sciguymjm.uberenchant.api.utils.Rarity;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityCombustEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootTable;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import wbs.enchants.WbsEnchantment;
 
 public class HellborneEnchant extends WbsEnchantment {
@@ -19,11 +23,7 @@ public class HellborneEnchant extends WbsEnchantment {
     }
 
     @EventHandler
-    public void onCombust(EntityCombustEvent event) {
-        if (event.getDuration() < 20) {
-            return;
-        }
-
+    public void onCombust(EntityDamageEvent event) {
         if (!(event.getEntity() instanceof LivingEntity living)) {
             return;
         }
@@ -36,12 +36,15 @@ public class HellborneEnchant extends WbsEnchantment {
         ItemStack chestItem = equipment.getChestplate();
 
         if (chestItem != null && containsEnchantment(chestItem)) {
-            int level = getLevel(chestItem);
+            int ticks = Math.max(20, living.getFireTicks());
 
-            PotionEffect strengthEffect = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, event.getDuration(), level - 1);
-            PotionEffect fireResEffect = new PotionEffect(PotionEffectType.FIRE_RESISTANCE, event.getDuration(), 0);
+            if (event.getCause() == EntityDamageEvent.DamageCause.FIRE_TICK) {
+                event.setCancelled(true);
+            }
+
+            int level = getLevel(chestItem);
+            PotionEffect strengthEffect = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, ticks, level - 1);
             living.addPotionEffect(strengthEffect);
-            living.addPotionEffect(fireResEffect);
         }
     }
 
@@ -83,12 +86,25 @@ public class HellborneEnchant extends WbsEnchantment {
 
     @Override
     public @NotNull String getDescription() {
-        return "You have strength and fire resistance while on fire, with the strength level relating to the " +
+        return "You have strength and immunity to fire tick damage, with strength level relating to the " +
                 "level of the enchantment.";
     }
 
     @Override
     public @NotNull String getTargetDescription() {
         return "Chestplate";
+    }
+
+    @Override
+    public @Nullable Double getAddToChance(LootTable table) {
+        NamespacedKey key = table.getKey();
+        if (!key.getNamespace().equalsIgnoreCase("incendium")) {
+            return null;
+        }
+
+        if (key.getKey().contains("armor") || key.getKey().contains("chestplate")) {
+            return 15.0;
+        }
+        return null;
     }
 }
