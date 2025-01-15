11712a03764600670511ba0c961a24f88af2f848
Weber588 <idonthavetotellyoumyname@gmail.com>
2025-01-15 19:04:26 +1100

    Buffed Aridity to be single leveled, but always auto-dry

--- src/main/java/wbs/enchants/enchantment/AridityEnchant.java
+++ src/main/java/wbs/enchants/enchantment/AridityEnchant.java
@@ -1,7 +1,6 @@
 package wbs.enchants.enchantment;
 
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,15 +13,10 @@ import wbs.enchants.WbsEnchants;
 import wbs.enchants.WbsEnchantsBootstrap;
 import wbs.enchants.enchantment.helper.NonPersistentBlockEnchant;
 import wbs.enchants.util.EntityUtils;
-import wbs.utils.util.WbsMath;
 
 public class AridityEnchant extends WbsEnchantment implements NonPersistentBlockEnchant {
-    // TODO: Make these configurable
-    private static final int MAX_LEVEL = 3;
-    private static final int CHANCE_PER_LEVEL = 100 / (MAX_LEVEL + 1);
-
     private static final String DEFAULT_DESCRIPTION = "A sponge enchantment that automatically returns it to your " +
-            "hand after placing, with a " + CHANCE_PER_LEVEL + "% chance per level of auto-drying immediately.";
+            "hand after placing, never getting wet!";
 
     public AridityEnchant() {
         super("aridity", DEFAULT_DESCRIPTION);
@@ -57,31 +51,15 @@ public class AridityEnchant extends WbsEnchantment implements NonPersistentBlock
         cloned.setAmount(1);
 
         Block block = event.getBlock();
-        boolean inNether = block.getWorld().getEnvironment() == World.Environment.NETHER;
-
-        if (inNether) {
-            cloned.setType(Material.SPONGE);
-        } else if (cloned.getType() == Material.SPONGE && !WbsMath.chance(CHANCE_PER_LEVEL * getLevel(placedItem))) {
-            int spongeRange = 7;
-            rangeLoop: for (int x = -spongeRange; x < spongeRange; x++) {
-                for (int y = -spongeRange; y < spongeRange; y++) {
-                    for (int z = -spongeRange; z < spongeRange; z++) {
-                        if (Math.abs(x) + Math.abs(y) + Math.abs(z) <= spongeRange) {
-                            if (block.getLocation().clone().add(x, y, z).getBlock().getType() == Material.WATER) {
-                                cloned.setType(Material.WET_SPONGE);
-                                break rangeLoop;
-                            }
-                        }
-                    }
-                }
-            }
-        }
 
         // Wait until next tick
         WbsEnchants.getInstance().runSync(() -> {
-            EntityUtils.giveSafely(player, cloned);
+            if (block.getType() != Material.SPONGE && block.getType() != Material.WET_SPONGE) {
+                return;
+            }
 
-            event.getBlock().setType(Material.AIR);
+            EntityUtils.giveSafely(player, cloned);
+            block.setType(Material.AIR);
         });
     }
 }
