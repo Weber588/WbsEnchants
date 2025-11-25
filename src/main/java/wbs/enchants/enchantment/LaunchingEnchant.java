c47a5fe7f1172ddcea3eaaecb27c724716af8d06
Weber588 <idonthavetotellyoumyname@gmail.com>
2025-11-25 16:25:33 +1100

    Made much of Launching configurable

--- src/main/java/wbs/enchants/enchantment/LaunchingEnchant.java
+++ src/main/java/wbs/enchants/enchantment/LaunchingEnchant.java
@@ -10,6 +10,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.phys.Vec3;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.craftbukkit.util.CraftLocation;
 import org.bukkit.entity.Player;
@@ -31,9 +32,11 @@ public class LaunchingEnchant extends WbsEnchantment {
     private static final NamespacedKey COOLDOWN = WbsEnchantsBootstrap.createKey("launch_cooldown");
 
     private static final @NotNull String DEFAULT_DESCRIPTION = "Gives you an upwards boost when you start gliding while sneaking.";
-    // TODO: Make this configurable
-    public static final double SPEED_PER_LEVEL = 0.6;
-    public static final double COOLDOWN_SECONDS = 5;
+
+    public double speedPerLevel = 0.6;
+    public double cooldownSeconds = 5;
+    public double maxBlocksAboveGround = 2;
+    public int moveCheckBypassTicks = 5;
 
     public LaunchingEnchant() {
         super("launching", DEFAULT_DESCRIPTION);
@@ -43,6 +46,16 @@ public class LaunchingEnchant extends WbsEnchantment {
                 .maxLevel(3)
                 .weight(1)
                 .anvilCost(4);
+    }
+
+    @Override
+    public void configure(ConfigurationSection section, String directory) {
+        super.configure(section, directory);
+
+        speedPerLevel = section.getDouble("speed-per-level", speedPerLevel);
+        cooldownSeconds = section.getDouble("cooldown-seconds", cooldownSeconds);
+        maxBlocksAboveGround = section.getDouble("max-blocks-above-ground", maxBlocksAboveGround);
+        moveCheckBypassTicks = section.getInt("move-check-bypass-ticks", moveCheckBypassTicks);
     }
 
     @EventHandler
@@ -63,8 +76,8 @@ public class LaunchingEnchant extends WbsEnchantment {
         if (highestEnchanted != null) {
             Location location = player.getLocation();
             // If off cooldown, start one. If on cooldown, check if player is within 5 blocks of the ground to do it anyway.
-            if (CooldownManager.newCooldown(player, (int) (COOLDOWN_SECONDS * Ticks.TICKS_PER_SECOND), COOLDOWN) ||
-                    location.getWorld().rayTraceBlocks(location, new Vector(0, -1, 0), 5) != null
+            if (location.getWorld().rayTraceBlocks(location, new Vector(0, -1, 0), maxBlocksAboveGround) != null &&
+                    CooldownManager.newCooldown(player, (int) (cooldownSeconds * Ticks.TICKS_PER_SECOND), COOLDOWN)
             ) {
                 // Force start the cooldown in case this was an override by being close to the ground
                 CooldownManager.startCooldown(player, COOLDOWN);
@@ -80,8 +93,8 @@ public class LaunchingEnchant extends WbsEnchantment {
                 ((CraftPlayer) player).getHandle().connection.send(
                         new ClientboundExplodePacket(
                                 CraftLocation.toVec3(location),
-                                Optional.of(new Vec3(0, level * SPEED_PER_LEVEL, 0)),
-                                ParticleTypes.CLOUD,
+                                Optional.of(new Vec3(0, level * speedPerLevel, 0)),
+                                ParticleTypes.GUST_EMITTER_SMALL,
                                 Holder.direct(SoundEvents.WIND_CHARGE_BURST.value())
                         )
                 );
@@ -90,7 +103,7 @@ public class LaunchingEnchant extends WbsEnchantment {
                     if (player.isValid()) {
                         player.getPersistentDataContainer().remove(IS_LAUNCHING);
                     }
-                }, 5);
+                }, moveCheckBypassTicks);
             }
         }
     }
@@ -105,7 +118,7 @@ public class LaunchingEnchant extends WbsEnchantment {
                 if (player.isValid()) {
                     player.getPersistentDataContainer().remove(IS_LAUNCHING);
                 }
-            }, 5);
+            }, moveCheckBypassTicks);
         }
     }
 }
